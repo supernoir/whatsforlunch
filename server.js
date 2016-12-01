96b747652e68c0c2acf51fcacb89a6d10ead395a
Conny <kawohl.bentodesign@gmail.com>
2016-12-01 17:56:50 +0100

    adds eslint and morgan

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-'use strict'
+'use strict';
 
 const express  = require('express');
 const app = express();
 const http = require('http');
-const fs = require('fs')  
+const fs = require('fs');  
+const morgan = require('morgan');
 
 
-let content;
+let content = {};
 
 try {  
-  content = fs.readFileSync('data.json', 'utf-8')
+	content = fs.readFileSync('data.json', 'utf-8');
 } catch (err) {
-  console.log(err)
+	console.log(err);
 }
-console.log(content)  
+console.log(content);  
 
 
-let getLunchOptions = () => {
-   console.log('Hello World');
+app.get('/', (req, res) => {
+	try {
+	res.send(content);
+} catch (err) {
+	res.send('Error');
 }
+});
 
-getLunchOptions();
 
 let port = 3344 || process.env.PORT;
-app.listen(port);
+
+app.listen(port, (err) => {  
+	if (err) {
+		return console.log('An Error occured: ', err);
+	}
+});
+
 console.log(`Server listening on ${port}`);
 
