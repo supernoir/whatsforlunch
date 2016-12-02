269fd159a4a60539a8b05cf82aff1bf9474c5184
Conny <kawohl.bentodesign@gmail.com>
2016-12-02 17:50:35 +0100

    implement api call to giphy, fetching rand gif

--- Colorball.js
+++ Colorball.js
@@ -10,7 +10,7 @@ let Colorball = () => {
 	return (
         <div id="colorball">
             <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="1" cy="1" r="3" id="ball" fill={RandomizeHex()} stroke="rgba(0,0,0,.2)" />
+                <circle cx="1" cy="1" r="3" id="ball" npfill={RandomizeHex()} stroke="rgba(0,0,0,.2)" />
             </svg>
         </div>
     );
--- index.js
+++ index.js
@@ -5,6 +5,40 @@ import ReactDOM from 'react-dom';
 //import Colorball from './Colorball';
 
 
+// Giphy fun!
+const giphy = require('giphy-api-without-credentials')();
+
+let gif = () => {
+    giphy.search({
+	        q: 'Pizza',
+	        rating: 'g'
+        }, function(err, res) {
+	        let randgif = res.data[0].images.downsized.url;
+            console.log(randgif);
+            return randgif
+        });
+}
+
+gif();
+
+
+
+class FoodGif extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            'gif' : 'http://i.imgur.com/LmXe2gA.gif'
+        }
+    }
+
+    render(){
+        return <div>
+        <img src={this.state.gif} id="foodpic" />
+        </div>
+    }
+}
+
+
 let Randomizer = () => {
 	return <div id="action"><a href="#" className="btn">Pick a Lunch Date!</a></div>;
 };
@@ -16,37 +50,38 @@ let LunchItem = () => {
 
 class Lunch extends React.Component{
 	constructor(props){
-	super(props);
-	this.state = {
-	'food': 'Burgers'
-    };
-}
+		super(props);
+		this.state = {
+		'food': 'Burgers'
+	};
+	}
 
 	componentDidMount(){
-	fetch('http://127.0.0.1:3344/', {
+		fetch('http://127.0.0.1:3344/', {
 	        method: 'get'
-}).then(function(res) {
+	}).then(function(res) {
 	res.json().then(function(data) {  
-	console.log(data);
-	return data;
-});  
+		console.log(data);
+		return data;
+	}); 
 }).catch(function(err) {
 	        console.log(err);
 });
-	console.log(this.res);
 
-}
+
+	}
 
 	render(){
-	return(
+		return(
         <div id="content">
         <h1>What's for lunch?</h1>
         <p>{this.state.food}</p>
+        <FoodGif />
         <LunchItem/>
         <Randomizer />
         </div>
-        );
-}
+	);
+	}
 }
 
 
