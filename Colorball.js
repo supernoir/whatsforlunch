78236ed1a33d8c910a90af8921203fb70a6caf7b
Conny <kawohl.bentodesign@gmail.com>
2016-12-02 17:03:24 +0100

    takes colorballs out for now

---
+++ Colorball.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+let RandomizeHex = () => {
+	let color = '#'+Math.floor(Math.random()*16777215).toString(16);
+	return color;
+};
+
+let Colorball = () => {
+	return (
+        <div id="colorball">
+            <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="1" cy="1" r="3" id="ball" fill={RandomizeHex()} stroke="rgba(0,0,0,.2)" />
+            </svg>
+        </div>
+    );
+};
+
+
+
+console.log('Baaaallls');
+
+export default Colorball;
--- index.js
+++ index.js
@@ -2,38 +2,51 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Colorball from './Colorball';
+
 
 let Randomizer = () => {
-    return <div id="action"><a href="#" className="btn">Pick a Lunch Date!</a></div>
-}
+	return <div id="action"><a href="#" className="btn">Pick a Lunch Date!</a></div>;
+};
 
 let LunchItem = () => {
-    return(<div id="lunchItem">Pizza</div>);
-}
+	return <div id="lunchItem"></div>;
+};
+
 
 class Lunch extends React.Component{
-    componentDidMount(){
-        fetch('http://127.0.0.1:3344/', {
-	        method: 'get'
-        }).then(function(res) {
-       res.json().then(function(data) {  
-        console.log(data);
-        return data;
-      });  
-        }).catch(function(err) {
-	        console.log(err);
-        });
-    }
+	constructor(props){
+	super(props);
+	this.state = {
+	'food': 'Burgers'
+    };
+}
 
-    render(){
-    return(
+	componentDidMount(){
+	fetch('http://127.0.0.1:3344/', {
+	        method: 'get'
+}).then(function(res) {
+	res.json().then(function(data) {  
+	console.log(data);
+	return data;
+});  
+}).catch(function(err) {
+	        console.log(err);
+});
+	console.log(this.res);
+
+}
+
+	render(){
+	return(
         <div id="content">
         <h1>What's for lunch?</h1>
-        <LunchItem />
+        <p>{this.state.food}</p>
+        <LunchItem/>
         <Randomizer />
         </div>
         );
-    }
+}
 }
 
 
